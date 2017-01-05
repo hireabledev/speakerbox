3dbbb6b290b71ac50768bfa3d7254bcae9ac4c66
Marius Craciunoiu <marius@craciunoiu.net>
2017-01-04 21:30:15 -0700

    Make fetch delay fully configurable through env vars

--- src/worker/utils.js
+++ src/worker/utils.js
@@ -1,9 +1,11 @@
 import moment from 'moment';
+import { FB_FETCH_DELAY, TWITTER_FETCH_DELAY, LINKEDIN_FETCH_DELAY } from 'lib/config';
 
 export function syncedRecently(account) {
   if (!account.synced) {
     return false;
   }
-  const fifteenMinutesAgo = moment().subtract(10, 'minutes');
-  return moment(account.synced).isAfter(fifteenMinutesAgo);
+  const ms = Math.min(FB_FETCH_DELAY, TWITTER_FETCH_DELAY, LINKEDIN_FETCH_DELAY);
+  const time = moment().subtract(ms, 'ms');
+  return moment(account.synced).isAfter(time);
 }
