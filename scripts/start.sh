b7ee0ce975dabd38b1697853ff15c5f7f5d9a1c6
Marius Craciunoiu <marius@craciunoiu.net>
2017-01-26 10:23:50 -0700

    Fix GC memory usage on heroku

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-if [[ $NODE_ENV == 'production' ]];
+# https://github.com/damianmr/heroku-node-settings/blob/master/heroku-node-settings.sh
+if [[ ! "$WEB_MEMORY" == "" ]]; then
+  if [[ $WEB_MEMORY -le 512 ]]; then
+    NODE_FLAGS="--max_semi_space_size=2 --max_old_space_size=256 --max_executable_size=192"
+  elif [[ $WEB_MEMORY -le 768 ]]; then
+    NODE_FLAGS="--max_semi_space_size=8 --max_old_space_size=512 --max_executable_size=384"
+  elif [[ $WEB_MEMORY -le 1024 ]]; then
+    NODE_FLAGS="--max_semi_space_size=16 --max_old_space_size=1024 --max_executable_size=512"
+  fi
+fi
+
+if [[ $NODE_ENV == "production" ]];
 then
-  node build/index.js
+  echo "Starting app with command:"
+  echo " " node $NODE_FLAGS "$@"
+  node $NODE_FLAGS -- build/index.js
 else
   nodemon --config .nodemon --exec babel-node --no-babelrc --plugins transform-es2015-modules-commonjs,transform-runtime --presets stage-3,react -- src/index.js
 fi
