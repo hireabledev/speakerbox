6d34f97ec2c1701bd5ab68008e1570027998539b
Marius Craciunoiu <marius@craciunoiu.net>
2017-01-26 22:10:38 -0700

    Optimize memory for hobby dyno

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@
 
 if [[ $NODE_ENV == 'production' ]];
 then
-  node build/index.js
+  node --optimize_for_size --max_old_space_size=460 --gc_interval=100 build/index.js
 else
   BABEL_ENV=development nodemon --config .nodemon --exec babel-node -- src/index.js
 fi
