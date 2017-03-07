943f4c185a5e0df974961e945cee2f15aca1f7bf
Marius Craciunoiu <marius@craciunoiu.net>
2017-03-06 20:40:04 -0700

    Truncate and format RSS posts

--- src/dashboard/client/components/post/rss-post.jsx
+++ src/dashboard/client/components/post/rss-post.jsx
@@ -1,4 +1,6 @@
 import React, { PropTypes } from 'react';
+import trim from 'lodash/trim';
+import truncate from 'lodash/truncate';
 import Post from './post';
 import PostActions from '../post-actions';
 import PostFavoriteButton from '../post-favorite-button';
@@ -6,11 +8,23 @@ import PostScheduleButton from '../post-schedule-button';
 import PostViewLink from '../post-view-link';
 
 export default function RSSPost({ post, waypoint }) {
+  let text = post.message;
+
+  try {
+    const fragment = document.createDocumentFragment();
+    const div = document.createElement('div');
+    div.innerHTML = post.message;
+    fragment.appendChild(div);
+    text = truncate(trim(fragment.textContent), { length: 300 });
+  } catch (err) {
+    console.warn('Could not get RSS Post text.');
+  }
+
   return (
     <Post
       post={{
         ...post,
-        message: (<span dangerouslySetInnerHTML={{ __html: post.message }} />),
+        message: (<span dangerouslySetInnerHTML={{ __html: text }} />),
       }}
       waypoint={waypoint}
       actions={(
