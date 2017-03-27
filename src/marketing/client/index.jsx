9ab73c8e277e018027d6f51f31bc0120fb18be70
Marius Craciunoiu <marius@craciunoiu.net>
2017-03-27 16:50:12 -0600

    Track all clicks on marketing app

--- src/marketing/client/index.jsx
+++ src/marketing/client/index.jsx
@@ -1,1 +1,10 @@
 import './styles.scss';
+
+document.addEventListener('click', e => {
+  if (e.target.href) {
+    mixpanel.track('Visitor clicked marketing link', {
+      text: e.target.textContent.trim(),
+      url: e.target.href,
+    });
+  }
+});
