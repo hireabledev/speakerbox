867d9aae0f7dc07f95b0941e7a811b44f7f71200
Marius Craciunoiu <marius@craciunoiu.net>
2017-04-04 20:54:34 -0600

    Track SSO page clicks and impressions

--- src/sso/routes/home-page.js
+++ src/sso/routes/home-page.js
@@ -1,4 +1,7 @@
+import mixpanel from 'lib/mixpanel';
+
 export default function homePageRoute(req, res, next) {
+  mixpanel.track('SSO page viewed', { distinct_id: req.user ? req.user.id : req.session.id });
   res.render('home-page.njk', {
     description: 'Single Sign On',
   });
